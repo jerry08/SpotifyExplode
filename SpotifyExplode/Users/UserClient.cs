fcaa438de08aaac5732673600280632d7e82c1cd
Juan Couste <juancouste28@gmail.com>
2024-08-26 21:41:32 -0300

    UserId changes

--- SpotifyExplode/Users/UserClient.cs
+++ SpotifyExplode/Users/UserClient.cs
@@ -1,8 +1,10 @@
-﻿using System.Net.Http;
+﻿using SpotifyExplode.Utils;
+using System;
+using System.Net.Http;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using SpotifyExplode.Utils;
 
 namespace SpotifyExplode.Users;
 
@@ -12,19 +14,25 @@ namespace SpotifyExplode.Users;
 /// <remarks>
 /// Initializes an instance of <see cref="UserClient" />.
 /// </remarks>
-public class UserClient(HttpClient http)
-{
+public class UserClient(HttpClient http) {
+    private static readonly Regex UserUrlRegex = new Regex(@"^(https?://)?open.spotify.com/user/([^\/]*)/?");
+
     private readonly SpotifyHttp _spotifyHttp = new(http);
 
     /// <summary>
     /// Gets the metadata associated with the specified user.
     /// </summary>
     public async ValueTask<User> GetAsync(
-        UserId userId,
-        CancellationToken cancellationToken = default)
-    {
+        string userIdOrUrl,
+        CancellationToken cancellationToken = default) {
+        // Regular URL
+        // https://open.spotify.com/user/xxu0yww90v07gbh9veqta7ze0
+        Match match = UserUrlRegex.Match(userIdOrUrl);
+        if (match.Success)
+            userIdOrUrl = Uri.UnescapeDataString(match.Groups[match.Groups.Count - 1].Value);
+
         var response = await _spotifyHttp.GetAsync(
-            $"https://api.spotify.com/v1/users/{userId}",
+            $"https://api.spotify.com/v1/users/{userIdOrUrl}",
             cancellationToken
         );
 
